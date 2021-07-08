027c708355acc4014d8de7261fae49d9da4bed1f
origin <chldlsrb1000@naver.com>
2021-07-08 19:23:00 +0900

    Initial commit

---
+++ src/main/java/org/mit2/randomluckygames/RandomLuckyGamesApplication.java
@@ -0,0 +1,13 @@
+package org.mit2.randomluckygames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomLuckyGamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomLuckyGamesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mit2/randomluckygames/RandomLuckyGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mit2.randomluckygames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomLuckyGamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
